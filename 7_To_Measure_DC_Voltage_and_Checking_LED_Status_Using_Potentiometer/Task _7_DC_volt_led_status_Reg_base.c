fc67b3938069ba97b6189baf8dca18dea14535d7
ANTU ROY <vandemataram15aug1947@gmail.com>
2024-06-25 22:20:45 +0530

    _base Commit.

---
+++ 7_To_Measure_DC_Voltage_and_Checking_LED_Status_Using_Potentiometer/Task _7_DC_volt_led_status_Reg_base.c
@@ -0,0 +1,157 @@
+/*Maaz khan
+ * Reg No : 19jzele0320
+***************************************************************************************
+  DC Voltage measurement system through potentiometer
+  when voltage is less than 2 volt than RED LED should toggle
+  when voltage greater than 2 volt it should turn off*
+***************************************************************************************/
+//header files
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "F2837xD_device.h"
+#include "F28x_Project.h"
+#include "F2837xD_Examples.h"
+#include "driverlib.h"
+#include "device.h"
+
+#define LED_1 GpioDataRegs.GPBDAT.bit.GPIO34
+//12 Bit ADC
+#define     EX_ADC_RESOLUTION       12
+#define ADCA_BASE                 0x00007400U
+
+// Function prototype
+void ConfigADC();
+void initADC_SOC(void);
+void Timer_Delay();
+
+/////// MAAZ KHAN   ////////////////
+uint16_t Adc_Result_1;
+float ADCINA0_1A,Current;
+
+void main(void)
+{
+    /////// MAAZ KHAN   ////////////////
+
+    // Initialize all particular clock
+    Device_init();
+    // Initialize Gpio's
+    Device_initGPIO();
+
+    Interrupt_initModule();
+    // Initialize Pie Vector Table
+    Interrupt_initVectorTable();
+
+    /////// MAAZ KHAN   ////////////////
+    EALLOW;
+    GpioCtrlRegs.GPBDIR.bit.GPIO34 = 1;
+    GpioCtrlRegs.GPBMUX1.bit.GPIO34 = 0;
+    EDIS;
+    // Calling functions in main
+    ConfigADC();
+    initADC_SOC();
+    EINT;
+    ERTM;
+
+    while (1)
+    {
+        /////// MAAZ KHAN   ////////////////
+
+        // Convert, wait for completion, and store results
+        AdcaRegs.ADCSOCFRC1.bit.SOC0 = 1;
+
+
+        while (AdcaRegs.ADCINTFLG.bit.ADCINT1 == false)
+        {
+
+        }
+        // Clear ADC interrupt flag
+        AdcaRegs.ADCINTFLGCLR.bit.ADCINT1 = 1;
+
+        // Store results
+        Adc_Result_1 = AdcaResultRegs.ADCRESULT0;
+        // If ADC value is less than 2volt then toggle the LED1
+        if(Adc_Result_1 < 2000){
+            LED_1=1;
+            Timer_Delay();
+            LED_1=0;
+            Timer_Delay();
+        }
+        else
+            LED_1 = 0;
+        ADCINA0_1A = Adc_Result_1*(3.3/4096);
+
+
+
+    }
+}
+
+/////// MAAZ KHAN   ////////////////
+
+void ConfigADC()
+{
+    /////// MAAZ KHAN   ////////////////
+    EALLOW;
+    // Configure ADC clock divider
+    AdcaRegs.ADCCTL2.bit.PRESCALE = 6;
+
+    // Configure ADC resolution in 12 bit mode which is single ended
+#if(EX_ADC_RESOLUTION == 12)
+    {
+        // 12-bit resolution
+        AdcaRegs.ADCCTL2.bit.RESOLUTION = 0; // 12-bit resolution
+    }
+    // Differential mode 16 bit
+#elif(EX_ADC_RESOLUTION == 16)
+    {
+        // 16-bit resolution
+        AdcaRegs.ADCCTL2.bit.RESOLUTION = 1;
+    }
+#endif
+    //AdcaRegs.ADCCTL2.bit.PRESCALE = 6; // Clock divider for ADCCLK is 4
+
+    // Configure ADC interrupt pulse generation
+    AdcaRegs.ADCCTL1.bit.INTPULSEPOS = 1; // End of conversion pulse
+
+    // Enable ADC module
+    AdcaRegs.ADCCTL1.bit.ADCPWDNZ= 1;
+
+    // Delay for 1 ms
+    DELAY_US(1000);
+    EDIS;
+}
+
+/////// MAAZ KHAN   ////////////////
+
+void initADC_SOC(void)
+{
+    // Configure SOC0 for ADCINA0
+    EALLOW;
+    // Trigger SOC0 using software delay
+    AdcaRegs.ADCSOC0CTL.bit.TRIGSEL = 0;
+    // Channel selection for SOC0
+    AdcaRegs.ADCSOC0CTL.bit.CHSEL = 0;
+    // Sample window is 14 SYSCLK cycles , in Acquisition Phase
+    AdcaRegs.ADCSOC0CTL.bit.ACQPS = 14;
+    // Enable Force1 Register for SOC0
+    AdcaRegs.ADCSOCFRC1.all = 0x155;
+
+    AdcaRegs.ADCINTSEL1N2.bit.INT1SEL = 0;
+    // Enable interrupt
+    AdcaRegs.ADCINTSEL1N2.bit.INT1E = 1;
+    // Clear interrupt flag
+    AdcaRegs.ADCINTFLGCLR.bit.ADCINT1 = 1;
+    EDIS;
+}
+
+void Timer_Delay()
+{
+    // Initialization of Cpu Timers
+    InitCpuTimers();
+    // Configure Timer 0 with a period of DELAY 500 milliseconds
+    ConfigCpuTimer(&CpuTimer0, 200, 100000);
+    // Start Timer 0
+    StartCpuTimer0();
+    // Wait until Timer 0 times out
+    while (CpuTimer0Regs.TCR.bit.TIF == 0);
+}
